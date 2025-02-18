7b7713c5a57f1d3192d9837ea545f8b30415b0cf
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-02-18 14:52:54 +0200

    hw9

--- js/task-2.js
+++ js/task-2.js
@@ -12,12 +12,66 @@
 // console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
 // console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a felis in nunc fringilla"
 
-const heading = document.createElement('h1');
-headding.classList.add('title');
-heading.textContent = 'This is a heading';
-console.log(heading); // <h1 class="title">This is a heading</h1>
+// const heading = document.createElement('h1');
+// headding.classList.add('title');
+// heading.textContent = 'This is a heading';
+// console.log(heading); // <h1 class="title">This is a heading</h1>
 
-const image = document.createElement('img');
-image.src = 'https://picsum.photos/id/11/320/240';
-image.alt = 'Nature';
-console.log(image); // <img src="https://picsum.photos/id/11/320/240" alt="Nature" />
+// const image = document.createElement('img');
+// image.src = 'https://picsum.photos/id/11/320/240';
+// image.alt = 'Nature';
+// console.log(image); // <img src="https://picsum.photos/id/11/320/240" alt="Nature" />
+
+// const dog = {
+//   name: 'Mango',
+//   age: 3,
+//   isGoodBoy: true,
+// };
+
+// const json = JSON.stringify(dog);
+// console.log(json); // '{"name":"Mango","age":3,"isGoodBoy":true}'
+
+// const data = JSON.parse('Well, this is awkward');
+// console.log("❌ You won't see this log");
+
+// try {
+//   const data = JSON.parse('Well, this is awkward');
+// } catch (error) {
+//   console.log(error.name); // "SyntaxError"
+//   console.log(error.message); // Unexpected token W in JSON at position 0
+// }
+
+// console.log('✅ This is fine, we handled parsing error in try...catch');
+
+// localStorage.setItem('ui-theme', 'light');
+// console.log(localStorage); // Storage {ui-theme: "light", length: 1}
+
+// const settings = {
+//   theme: 'dark',
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
+
+// localStorage.setItem('settings', JSON.stringify(settings));
+
+// sessionStorage.setItem('user-id', '123');
+// sessionStorage.setItem('tickets', JSON.stringify({ from: 'Lviv', to: 'Kyiv', quantity: 2 }));
+// console.log(sessionStorage);
+// // Storage {user-id: '123', tickets: '{"from":"Lviv","to":"Kyiv","quantity":2}', length: 2}
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.elements.message;
+const localStorageKey = 'goit-example-message';
+
+textarea.value = localStorage.getItem(localStorageKey) ?? '';
+
+form.addEventListener('input', evt => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
